e075ef31db6bcaf88588451f6edffe7f4972aeb2
Waly <ahmadouwalyndiaye@gmail.com>
2023-05-01 20:59:04 +0000

    Implement show vente

--- assets/app.js
+++ assets/app.js
@@ -17,53 +17,74 @@ import "./bootstrap";
 import "./modal";
 
 window.notyf = new Notyf();
-const actions = [...document.querySelectorAll("[name='vente']")].map($form => $form.action);
 
 document.addEventListener("submit", async (e) => {
-  const form = e.target;
-  const action = form.action;
-  if (actions.includes(action)) {
-    e.preventDefault();
-    const response = await fetch(action, {
-      method: "post",
-      body: new FormData(form),
-    });
-    if (!response.ok) {
-      notyf.error("Une erreur est survenue.");
-      return;
+    const actions = [...document.querySelectorAll("[name='vente']")].map($form => $form.action);
+    const form = e.target;
+    const action = form.action;
+    if (actions.includes(action)) {
+        e.preventDefault();
+        const response = await fetch(action, {
+            method: "post",
+            body: new FormData(form),
+        });
+        if (response.redirected) {
+            notyf.success("La vente a ete enregistree.");
+            window.location.href = response.url;
+        } else {
+            form.outerHTML = await response.text();
+        }
     }
-    if (response.redirected) {
-      notyf.success("La vente a ete enregistree.");
-      window.location.href = response.url;
-    } else {
-      form.outerHTML = await response.text();
-    }
-  }
 });
 
 const $modalOpenerBtn = document.querySelectorAll(
-  '[data-target="modal-edit-vente"][data-vente-id]'
+    '[data-target="modal-edit-vente"][data-vente-id]'
 );
 
 $modalOpenerBtn.forEach(($modalOpener) => {
-  $modalOpener.addEventListener("click", async (e) => {
-    const { venteId } = $modalOpener.dataset;
-    //get the form
-    const response = await fetch(`/vente/${venteId}/edit`);
-    const $form = await response.text();
+    $modalOpener.addEventListener("click", async (e) => {
+        const {venteId} = $modalOpener.dataset;
+        const response = await fetch(`/vente/${venteId}/edit`);
+        const $form = await response.text();
 
-    if (!response.ok) {
-      notyf.error("Une erreur est survenue.");
-      return;
-    }
+        if (!response.ok) {
+            notyf.error("Une erreur est survenue.");
+            return;
+        }
 
-    document
-      .querySelectorAll("#modal-edit-vente article form")
-      .forEach(($form) => {
-        $form.remove();
-      });
-    document
-      .querySelector("#modal-edit-vente article")
-      .insertAdjacentHTML("beforeend", $form);
-  });
+        document
+            .querySelectorAll("#modal-edit-vente article form")
+            .forEach(($form) => {
+                $form.remove();
+            });
+        document
+            .querySelector("#modal-edit-vente article")
+            .insertAdjacentHTML("beforeend", $form);
+    });
+});
+
+const $modalDetailVenteOpenerBtn = document.querySelectorAll(
+    '[data-target="modal-detail-vente"][data-vente-id]'
+);
+
+$modalDetailVenteOpenerBtn.forEach(($modalDetailVenteOpener) => {
+    $modalDetailVenteOpener.addEventListener("click", async (e) => {
+        const {venteId} = $modalDetailVenteOpener.dataset;
+        const response = await fetch(`/vente/${venteId}`);
+        const $form = await response.text();
+
+        if (!response.ok) {
+            notyf.error("Une erreur est survenue.");
+            return;
+        }
+
+        document
+            .querySelectorAll("#modal-detail-vente article table")
+            .forEach(($form) => {
+                $form.remove();
+            });
+        document
+            .querySelector("#modal-detail-vente article")
+            .insertAdjacentHTML("beforeend", $form);
+    });
 });
